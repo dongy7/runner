c84f1f3ec2cdda5a3bd283cf2cfe49278f1374e8
Coalman <tehcoalman@gmail.com>
2014-01-08 13:06:35 -0800

    Added assets and getting ready for dev.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,12 +6,6 @@ public class PlayerScript : MonoBehaviour {
 	public Vector2 speed = new Vector2(3, 3);
 	Vector2 movement;
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		var inputX = Input.GetAxis ("Horizontal");
 		var inputY = Input.GetAxis ("Vertical");
